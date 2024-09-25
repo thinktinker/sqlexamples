1aec218fdd18f05c55928f8866eda6be58fcf8b0
Martin Leong <martinleongmo@gmail.com>
2024-09-25 15:21:54 +0800

    Update c_structure_volunteersdb.sql

--- c_structure_volunteersdb.sql
+++ c_structure_volunteersdb.sql
@@ -15,93 +15,85 @@ USE volunteersdb;
 -- In the FIRST NORMAL, we create separate tables for columns cities and languages 
 -- that are not functionally dependent to the each volunteer record
 
--- Drop the cities table if it exists
-DROP TABLE IF EXISTS cities;
+-- Active: 1715564057998@@127.0.0.1@3306
+CREATE DATABASE volunteersdb
+    DEFAULT CHARACTER SET = 'utf8mb4';
+
+USE volunteersdb;
 
 -- Create the cities table
 -- volunteers table will separately refer to this table via city.id to obtain name of the city
-CREATE TABLE cities (
+CREATE TABLE IF NOT EXISTS cities (
   id INT NOT NULL AUTO_INCREMENT,
   city VARCHAR(100) NOT NULL,
   PRIMARY KEY (id)
 );
 
--- Insert values to the cities table
+-- Insert values to cities tables
 INSERT INTO cities (id, city) VALUES
-(1, 'London'),
-(2, 'Bristol'),
-(3, 'Hove');
-
--- Drop the languages table if it exists
-DROP TABLE IF EXISTS languages;
+(1, "London"),
+(2, "Bristol"),
+(3, "Hove");
 
 -- Create the languages table
--- The volunteers table will refer to a pivot table (volunteers_languages, below) via volunteer_id 
--- As one volunteer may speak one or more languages
--- And one language may be spoken by one or more volunteer
-CREATE TABLE languages (
-  id int NOT NULL AUTO_INCREMENT,
-  language varchar(30) NOT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS languages (
+    id INT NOT NULL AUTO_INCREMENT,
+    language VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
--- Insert values to the languages table
-INSERT INTO languages (id, language) VALUES
-(1, 'German'),
-(2, 'English'),
-(3, 'Dutch');
+-- Insert values to languages table
+INSERT INTO LANGUAGES (id, language) VALUES
+(1, "German"),
+(2, "English"),
+(3, "Dutch");
 
--- Drop the volunteers table if it exists
-DROP TABLE IF EXISTS volunteers;
-
--- b) 
--- Create the volunteers table in its FIRST AND SECOND normal form
--- TO BE UPDATED
-CREATE TABLE volunteers (
-  id INT NOT NULL AUTO_INCREMENT,
-  surname VARCHAR(50) NOT NULL,
-  mobile VARCHAR(15) NOT NULL,
-  city_id INT NOT NULL,
-  PRIMARY KEY (id)
+-- Create the volunteers table
+CREATE TABLE IF NOT EXISTS volunteers (
+    id INT NOT NULL AUTO_INCREMENT,
+    surname VARCHAR(50) NOT NULL,
+    mobile VARCHAR(15) NOT NULL,
+    city_id INT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_volunteercity FOREIGN KEY (city_id) REFERENCES cities(id)
 );
 
+-- Insert values to volunteers table
 INSERT INTO volunteers (surname, mobile, city_id) VALUES
-('Kroner', '020 1234 5678',  1),
-('James', '020 5678 1234', 2),
-('Dexter', '020 7654 4321', 3),
-('Stephen', '020 4321 8765', 1);
+('Kroner', '020 1234 5678',  1),    -- London
+('James', '020 5678 1234', 2),      -- Bristol
+('Dexter', '020 7654 4321', 3),     -- Hove
+('Stephen', '020 4321 8765', 1);    -- London
 
-ALTER TABLE volunteers 
-ADD CONSTRAINT fk_volunteerscity FOREIGN KEY (city_id) REFERENCES cities(id);
-
-
--- Saluation is also not functionally dependent of the columns in volunteers
-DROP TABLE IF EXISTS salutations;
-
+-- Create the salutations table
 CREATE TABLE salutations (
     id INT NOT NULL AUTO_INCREMENT,
     salutation VARCHAR(10) NOT NULL,
     PRIMARY KEY(id)
 );
 
+-- Insert values to salutations table
 INSERT INTO salutations (id, salutation) VALUES
 (1, 'Mr'),
 (2, 'Miss'),
 (3, 'Mrs');
 
+-- Alter table volunteers to include salutation_id
 ALTER TABLE volunteers ADD COLUMN salutation_id INT NOT NULL AFTER id;
 
+-- Insert values of salutation_id to each volunteer
 UPDATE volunteers SET salutation_id = 1 WHERE (id = 1);
 UPDATE volunteers SET salutation_id = 3 WHERE (id = 2);
 UPDATE volunteers SET salutation_id = 2 WHERE (id = 3);
 UPDATE volunteers SET salutation_id = 1 WHERE (id = 4);
 
-ALTER TABLE volunteers ADD CONSTRAINT fk_volunteerssalutations FOREIGN KEY (salutation_id) REFERENCES salutations(id);
+-- Add constrain, where volunteers table saluation_id reference salutations table's id
+ALTER TABLE volunteers
+ADD CONSTRAINT fk_volunteerssalutations FOREIGN KEY (salutation_id) 
+REFERENCES salutations(id);
 
--- Create a relationship table (many-many) between volunteers and the language(s) they speak 
-DROP TABLE IF EXISTS volunteers_languages;
-
-CREATE TABLE volunteers_languages (
+-- Create a relationship table between volunteers and languages 
+CREATE TABLE IF NOT EXISTS volunteers_languages (
   volunteer_id INT NOT NULL,
   language_id INT NOT NULL,
   CONSTRAINT fk_volunteerlang FOREIGN KEY (volunteer_id) REFERENCES volunteers(id), 
@@ -110,30 +102,29 @@ CREATE TABLE volunteers_languages (
 );
 
 INSERT INTO volunteers_languages (volunteer_id, language_id) VALUES
-(1, 1),
-(1, 2),
-(2, 2),
-(3, 1),
-(3, 2),
-(3, 3),
-(4, 1);
+(1, 1), -- Kroner, German
+(1, 2), -- James, English
+(2, 2), -- James, English
+(3, 1), -- Dexter, German
+(3, 2), -- Dexter, English
+(3, 3), -- Dexter, Dutch
+(4, 1); -- Stephen, German
 
--- Create a table that records the hours put in by each volunteer 
-DROP TABLE IF EXISTS volunteer_hours;
 
-CREATE TABLE volunteer_hours (
-id INT NOT NULL AUTO_INCREMENT,
-volunteer_id INT NOT NULL,
-hours INT NOT NULL,
-created_at DATETIME ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-CONSTRAINT fk_volunteer FOREIGN KEY (volunteer_id) REFERENCES volunteers(id),
-PRIMARY KEY (id)
+-- Creat the table that records the hourse put in by each volunteer
+CREATE TABLE IF NOT EXISTS volunteer_hours(
+    id INT NOT NULL AUTO_INCREMENT,
+    volunteer_id INT NOT NULL,
+    hours INT NOT NULL,
+    created_at DATETIME ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_volunteer FOREIGN KEY (volunteer_id) REFERENCES volunteers(id),
+    PRIMARY KEY (id)
 );
 
 INSERT INTO volunteer_hours (volunteer_id, hours) VALUES
-(1, 15),
-(1, 12),
-(2, 32),
-(3, 11),
-(3, 7),
-(3, 5);
+(1, 15),    -- Kroner, 15 hours
+(1, 12),    -- Kroner, 15 hours
+(2, 32),    -- James, 32 hours
+(3, 11),    -- Dexter, 11 hours
+(3, 7),     -- Dexter, 7 hours
+(3, 5);     -- Dexter, 5 hours
