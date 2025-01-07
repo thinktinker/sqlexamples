9ab1184da9a28e36cccb967f76dd805fca93f58d
Martin Leong <martinleongmo@gmail.com>
2025-01-07 09:19:38 +0800

    Update a_customerdb.sql

--- a_customerdb.sql
+++ a_customerdb.sql
@@ -1,36 +1,52 @@
 CREATE DATABASE customerdb;
 
-# Create customer table
-CREATE TABLE customerdb.Customer(
-	CustomerID INT NOT NULL,
+USE customerdb;
+
+CREATE TABLE Customers(
+	CustomerID INT NOT NULL PRIMARY KEY,
     CustomerName VARCHAR(50) NOT NULL,
-    City VARCHAR(50) NOT NULL,
-    PRIMARY KEY(CustomerID)
+    City VARCHAR(50) NOT NULL
+    -- PRIMARY KEY (CustomerID)
 );
 
-# Create Order table that references Customer table (without a specific name to the constrain
-/*
-CREATE TABLE customerdb.Order(
-	OrderID INT NOT NULL,
+CREATE TABLE Orders(
+	OrderID INT NOT NULL PRIMARY KEY,
     CustomerID INT NOT NULL,
     ProductName VARCHAR(50) NOT NULL,
-    PRIMARY KEY(OrderID),
-    FOREIGN KEY(CustomerID) REFERENCES Customer(CustomerID)
+    -- PRIMARY KEY (OrderID)
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID) 
 );
-*/
 
-# Create Order table that refeences Customer table with a SPECIFIC constrain name
-/*
-CREATE TABLE customerdb.Order(
-	OrderID INT NOT NULL,
-    CustomerID INT NOT NULL,
-    ProductName VARCHAR(50) NOT NULL,
-    PRIMARY KEY(OrderID),
-    CONSTRAINT FK_CustomerOrder FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID) 
-);
-*/
+SHOW TABLES;
+SELECT * FROM customers;
 
-# Drop customerdb.Order table
-/* 
-DROP TABLE customerdb.Order;
-*/
+INSERT INTO Customers
+VALUES(1, "John Doe", "Cardiff");
+
+INSERT INTO Customers
+VALUES(2, "Sam Smith", "Oxford"),
+	  (3, "Jane Ayre", "Liverpool");
+
+ALTER TABLE Orders
+DROP FOREIGN KEY orders_ibfk_1;
+
+ALTER TABLE Customers
+MODIFY COLUMN CustomerID INT NOT NULL AUTO_INCREMENT;
+
+INSERT INTO Customers(CustomerName, City)	# Insert values to specific fields to a table
+VALUES("Mike Johnson", "Manchester");
+
+ALTER TABLE Orders
+MODIFY COLUMN OrderID INT AUTO_INCREMENT;
+
+ALTER TABLE Orders											# Re-establish the referential intergrity btwn Customers and Orders
+ADD CONSTRAINT fk_customer_id
+FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID);
+
+SELECT * FROM Customers;
+
+INSERT INTO Orders(CustomerID, ProductName)					# Insert orders for customers
+VALUES(1, "iPhone 16"),										# CustomerID must exist (referential integrity)
+	  (2, "iPad Air (Gen 4)");
+
+SELECT * FROM Orders;
