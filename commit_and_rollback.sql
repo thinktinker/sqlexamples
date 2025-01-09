62c80057cb547ab537156cde4283aa9b4a90c456
Martin Leong <martinleongmo@gmail.com>
2025-01-09 14:04:04 +0800

    Create commit_and_rollback.sql

---
+++ commit_and_rollback.sql
@@ -0,0 +1,48 @@
+USE testdb;
+
+CREATE TABLE users(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    email VARCHAR(255) UNIQUE
+);
+
+INSERT INTO users (first_name, last_name, email)
+VALUES ("John", "Doe", "johndoe@gmail.com");
+
+SET AUTOCOMMIT = 0;				-- turns off auto commit (so that one can roll back accidental changes)
+
+  DELETE FROM users				-- Scenario 1: I may have accidentally deleted a user
+WHERE id = 1;
+
+ROLLBACK;						      -- I decided to perform a rollback to undo the 'deletion'
+
+SELECT * FROM users;			-- Check the users is re-instated
+
+UPDATE users
+SET email = "johndoh@gmail.com"	-- Scenario 2: The email of the user is updated erroneously
+WHERE id = 1;
+
+ROLLBACK;						      -- Rollback to undo the changes
+
+UPDATE users
+SET email = "johndoe@yahoo.com"	-- Scenario 3: The email of the user is finally established correctly
+WHERE id = 1;
+
+COMMIT;							      -- The change in scenario 3 is committed (permanent)
+
+ROLLBACK;						      -- ONCE A COMMIT IS EXECUTED, any rollback is FULTILE
+
+								          -- HOW MANY ROLLBACKS CAN TAKE PLACE?
+
+UPDATE users					    -- Change #1
+SET last_name = "doh"
+WHERE id = 1;
+
+UPDATE users
+SET email = "johndoh@yahoo.com"	-- Chnage #2
+WHERE id = 1;
+
+ROLLBACK;						-- The ROLLBACK here will undo all previous changes unless a COMMIT is executed.
+
+
