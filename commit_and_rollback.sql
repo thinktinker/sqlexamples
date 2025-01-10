e7cc3ec5b284e2588c17ae088cf8c0f1f77e128a
Martin Leong <martinleongmo@gmail.com>
2025-01-10 09:29:41 +0800

    Update commit_and_rollback.sql

--- commit_and_rollback.sql
+++ commit_and_rollback.sql
@@ -10,39 +10,40 @@ CREATE TABLE users(
 INSERT INTO users (first_name, last_name, email)
 VALUES ("John", "Doe", "johndoe@gmail.com");
 
-SET AUTOCOMMIT = 0;				-- turns off auto commit (so that one can roll back accidental changes)
+SET AUTOCOMMIT = 0;						-- turns off auto commit (so that one can roll back accidental changes)
 
-  DELETE FROM users				-- Scenario 1: I may have accidentally deleted a user
+DELETE FROM users						-- Scenario 1: I may have accidentally deleted a user
 WHERE id = 1;
 
-ROLLBACK;						      -- I decided to perform a rollback to undo the 'deletion'
+ROLLBACK;							-- I decided to perform a rollback to undo the 'deletion'
 
-SELECT * FROM users;			-- Check the users is re-instated
+SELECT * FROM users;						-- Check the users is re-instated
 
 UPDATE users
 SET email = "johndoh@gmail.com"	-- Scenario 2: The email of the user is updated erroneously
 WHERE id = 1;
 
-ROLLBACK;						      -- Rollback to undo the changes
+ROLLBACK;							-- Rollback to undo the changes
 
 UPDATE users
-SET email = "johndoe@yahoo.com"	-- Scenario 3: The email of the user is finally established correctly
+SET email = "johndoe@yahoo.com"					-- Scenario 3: The email of the user is finally established correctly
 WHERE id = 1;
 
-COMMIT;							      -- The change in scenario 3 is committed (permanent)
+COMMIT;								-- The change in scenario 3 is committed (permanent)
 
-ROLLBACK;						      -- ONCE A COMMIT IS EXECUTED, any rollback is FULTILE
+ROLLBACK;							-- ONCE A COMMIT IS EXECUTED, any rollback is FULTILE
 
-								          -- HOW MANY ROLLBACKS CAN TAKE PLACE?
+								-- HOW MANY ROLLBACKS CAN TAKE PLACE?
 
-UPDATE users					    -- Change #1
+UPDATE users							-- Change #1
 SET last_name = "doh"
 WHERE id = 1;
 
-UPDATE users
-SET email = "johndoh@yahoo.com"	-- Chnage #2
+UPDATE users							-- Change #2
+SET email = "johndoh@yahoo.com"
 WHERE id = 1;
 
-ROLLBACK;						-- The ROLLBACK here will undo all previous changes unless a COMMIT is executed.
+ROLLBACK;							-- The ROLLBACK here will undo all previous changes unless a COMMIT is executed.
 
+SET AUTOCOMMIT = 1;						-- turn back on auto commit (so that changes and are permanent)
 
